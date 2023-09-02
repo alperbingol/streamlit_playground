27dd71c215293ae24940ff5d72fc1fe08034eb62
alperbingol <alperbingol@sabanciuniv.edu>
2023-09-02 15:19:43 +0200

    sd

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import streamlit as st
 from predict_page import show_predict_page
 
+import sys
+
+print("Python Version:", sys.version)
+st.write("Python Version:", sys.version)
+
 
 show_predict_page()
 
